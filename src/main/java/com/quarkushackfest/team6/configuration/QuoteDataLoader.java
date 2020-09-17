24047fd48775fc8de9abbc0df666d3014745a774
Himanshu Gupta <Himanshu.Gupta14@aexp.com>
2020-09-18 03:09:20 +0530

    core java change

--- src/main/java/com/quarkushackfest/team6/configuration/QuoteDataLoader.java
+++ src/main/java/com/quarkushackfest/team6/configuration/QuoteDataLoader.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.UUID;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class QuoteDataLoader {
@@ -39,7 +40,8 @@ public class QuoteDataLoader {
                         log.info("Going to insert: {}", l);
                         return quoteMongoReactiveRepository.persist(
                                 new Quote(idSupplier.get(), "El Quijote - " + UUID.randomUUID().toString(), l)).await().indefinitely();
-                    });
+                    })
+                    .collect(Collectors.toList());
             try {
                 bufferedReader.close();
             } catch (IOException e) {
