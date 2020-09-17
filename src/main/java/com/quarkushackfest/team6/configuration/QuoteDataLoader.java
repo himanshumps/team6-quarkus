b904da0b586b616427f2e8e385f742c42ba1580c
Himanshu Gupta <Himanshu.Gupta14@aexp.com>
2020-09-18 02:50:41 +0530

    addded logger for mongo check on startup

--- src/main/java/com/quarkushackfest/team6/configuration/QuoteDataLoader.java
+++ src/main/java/com/quarkushackfest/team6/configuration/QuoteDataLoader.java
@@ -5,6 +5,7 @@ import com.quarkushackfest.team6.repository.QuoteMongoReactiveRepository;
 import io.quarkus.runtime.StartupEvent;
 import io.smallrye.mutiny.Multi;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.UUID;
 import java.util.function.Supplier;
@@ -24,23 +25,23 @@ public class QuoteDataLoader {
   void run(@Observes StartupEvent ev) {
     log.info("Starting the application ...");
     // If there is no data in the database
-
-    if (quoteMongoReactiveRepository.count().await().asOptional().indefinitely().get() == 0L) {
+    log.info("quoteMongoReactiveRepository.count().await().indefinitely().intValue(): {}", quoteMongoReactiveRepository.count().await().indefinitely().intValue());
+    if (quoteMongoReactiveRepository.count().await().indefinitely().intValue() == 0) {
       var idSupplier = getIdSequenceSupplier();
       var bufferedReader =
           new BufferedReader(
               new InputStreamReader(getClass().getClassLoader().getResourceAsStream("pg2000.txt")));
-      Multi.createFrom()
-          .items(
-              bufferedReader
-                  .lines()
-                  .filter(l -> !l.trim().isEmpty())
-                  .map(
-                      l ->
-                          quoteMongoReactiveRepository.persist(
-                              new Quote(idSupplier.get(), "El Quijote - " + UUID.randomUUID().toString(), l)))).subscribe();
-      // .subscribe(m -> log.info("New quote loaded: {}", m.block()));
-      log.info("Repository contains now {} entries.", quoteMongoReactiveRepository.count().await());
+      bufferedReader
+              .lines()
+              .filter(l -> !l.trim().isEmpty())
+              .map(l -> quoteMongoReactiveRepository.persist(
+                      new Quote(idSupplier.get(), "El Quijote - " + UUID.randomUUID().toString(), l)).subscribe());
+      try {
+        bufferedReader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      log.info("Repository contains now {} entries.", quoteMongoReactiveRepository.count().await().indefinitely().intValue());
     } else {
       log.info("Data was already loaded in the database.");
     }
